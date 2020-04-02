00bc18fd70f4e3577efde8b120fb9d2630b899b5
Isaidhello <a.bye@student.fontys.nl>
2020-04-02 14:37:13 +0200

    Versie1
    Get out together

---
+++ Assets/Player1_movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1_movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//movement
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal1") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical1") * Time.deltaTime);
+    }
+}
